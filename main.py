ccca76f53d8cdd183b23596c42fd7bac9346ec87
Arlind Kamberi <arlindkamberi03@gmail.com>
2020-06-24 16:42:54 +0000

    Created a class for telephone bills

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ def calculator(a,b):
 calculator(3,4)
 
 print ("Hello")
+
+class TelephoneBills:
+    def __init__(self,billID,year,month,day,payvalue, name, company, paymentstatus, reminder):
+        self._ID = billID
+        self._paydate = (year,month,day)
+        self._payvalue = payvalue
+        self.name= name
+        self.company= company
+        self.paymentstatus= paymentstatus
+        self.reminder=reminder
