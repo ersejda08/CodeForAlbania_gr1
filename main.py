8bed6df6775be7bfb3fa2f3f0948350404ebdf15
Arber Kanan <semifireball@gmail.com>
2020-06-24 16:49:13 +0000

    i change the code

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-print("Hello Group1")
+#print("Hello Group1")
 
-def calculator(a,b):
-  Sum = a+b
-  product = a*b
-  print(Sum, product)
+#def calculator(a,b):
+ # Sum = a+b
+  #product = a*b
+  #print(Sum, product)
 
-calculator(3,4)
+#calculator(3,4)
+
+
+n = 10
+sum = 0
+for num in range(0, n+1, 1):
+    sum = sum+num
+print("SUM of first ", n, "numbers is: ", sum )
